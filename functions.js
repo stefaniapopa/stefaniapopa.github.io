94cd3fc9c766212d68d119d7fc9ffb78171aceb3
Stefania Popa <stefania_florina10@yahoo.com>
2020-11-12 19:30:03 +0200

    first version

---
+++ functions.js
@@ -0,0 +1,39 @@
+function hide(id){
+    // document.getElementById(id).style.display = 'none';
+    var el = document.getElementById(id);
+    console.info("hide" + id, el);
+    if (el){
+        el.style.display="none";
+    } else {
+        console.error("elementul nu exista");
+    }
+}
+
+
+function showHome(){
+    hide("languages");
+    hide("project");
+    hide("skills");
+    document.getElementById('home').style.display = '';
+}
+
+function showSkills(){
+    hide("home");
+    hide("project");
+    hide("languages");
+    document.getElementById('skills').style.display = '';
+}
+
+function showProjects(){
+    hide("skills");
+    hide("home");
+    hide("languages");
+    document.getElementById("project").style.display = "";
+}
+
+function showLanguages(){
+    document.getElementById("project").style.display = "none";
+    document.getElementById('skills').style.display = 'none';
+    document.getElementById('project').style.display = 'none';
+    document.getElementById("languages").style.display = "";
+}
