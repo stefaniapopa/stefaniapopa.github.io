63a46242ca76979fee3101086b8171a8a41f5e12
Stefania Popa <stefania_florina10@yahoo.com>
2020-11-16 21:33:49 +0200

    show all skills with js

--- functions.js
+++ functions.js
@@ -37,11 +37,17 @@ inItMenu();
 
 showPage("skills");
 
-var skills = [ "HTML", "CSS", "JavaScript", "Java"];
+var skills = [ 
+    "HTML",
+    "CSS", 
+    "JavaScript",
+    "Java"
+];
+
+var skillsLi = skills.map(function(skill){
+    return "<li>" + skill + "</li>";
+});
 
 //to doo add fauvorite skills
 var ul = document.querySelector("#skills ul");
-ul.innerHTML = "<li>" + skills[0] + "</li>"+
-                "<li>" + skills[1] + "</li>"+
-                "<li>" + skills[2] + "</li>"+
-                "<li>" + skills[3] + "</li>";
+ul.innerHTML =  skillsLi.join("");
